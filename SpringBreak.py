98ea3ccc67c6379b62996bde6db78116d8343437
Sami Tellez <samiratellezricardo16@gmail.com>
2017-03-14 00:12:35 -0400

    Some comments deleted

--- SpringBreak.py
+++ SpringBreak.py
@@ -2,7 +2,7 @@ n = raw_input ("Please enter your name: ")
 s = input ("Enter the year you were born: ")
 H = raw_input ("Enter the name of the college you're transfered from: " )
                
-#m = pow (s,2)
+
 
 
 import math
@@ -13,34 +13,24 @@ print "       "
 print ("Here your data for now: " + n)
 print ("Birth Year: " + `s`)
 print ("College transfering: " + H)
-#print `myList[0:2]` + " THIS JUST AN EXAMPLE OF HOW TO MAKE SLICING"
+
                
 print "----------------------------------------------------------------"
-#m = str (pow (s,2))
 
-#this is just a way
-#c  = str (math.sqrt(s))
 
-#print "The square of the year: " + m
-#print "Here the square root: " + `c`
 
-#minimum = min(myList)
-#imi = minimum in myList
-#print `mimi` + ">> The mim number in my list is " + `minimum`
 
-print "--------------------------------------------------------------"
-#newInput = input ("Enter the year you started school: ")
 newInput2 = input ("Enter the year your started at " + H + ": " )
 newInput3 = input ("Enter the year you drop school at " + H + ": ")
-#myList[0:] =
+
 myList.append(newInput2)
 myList.append(newInput3)
 
 
 print " "
-#print myList
+
 print " "
-#siblings=(">> " +`1999` + ". It\'s also the year when Ricky and Laura were born, Samira")
+
 
 if max(myList) in myList:
     print ( n + " you should bring your transcrips graded until " + `newInput3`)
@@ -49,13 +39,6 @@ else:
 
 print "  "
 
-#JUST TRYING
-#if myList.count (newInput) == 2: 
- #   print myList.append (" This year was witness of two important events in your life")
-#elif myList.count (newInput) == 1:
- #   print "That was actually an awesome year"
-#else:
- #   print " Well folk, 2013 is just once on your list . Just look at it, it\'s right below"
 
 print " "
 
@@ -80,8 +63,7 @@ print myList
 
 print "     "                                      
 
-#minimum = min(myList)
-#print "The minmum number of myList is: " + `minimum`
+
 
 raw_input ("Press <enter> to finalize the program")
 
